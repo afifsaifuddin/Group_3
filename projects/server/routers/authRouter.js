12549f9b154924958e163e5a52f965eb1998ffba
afifsaifuddin <55240789+afifsaifuddin@users.noreply.github.com>
2023-07-29 20:05:07 +0700

    register kasir (#5)

--- projects/server/routers/authRouter.js
+++ projects/server/routers/authRouter.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controller/authController");
-const { loginValidator, resultValidation } = require("../middleware/validator");
+const {
+  loginValidator,
+  resultValidation,
+  regisValidator,
+} = require("../middleware/validator");
+const { checkrole } = require("../middleware/checkrole");
 
 router.post("/login", loginValidator, resultValidation, authController.login);
 router.post("/forgot-password", authController.forgotPassword);
+router.post(
+  "/register",
+  regisValidator,
+  resultValidation,
+  authController.register
+);
 module.exports = router;
