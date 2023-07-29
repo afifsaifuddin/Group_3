bd6c6e30df1164726d4b93aa7b4f891d67f5ff94
Jaysons001 <Jasonyongker866@gmail.com>
2023-07-29 20:01:38 +0700

    Ini reset password

--- projects/server/routers/authRouter.js
+++ projects/server/routers/authRouter.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controller/authController");
 const { loginValidator, resultValidation } = require("../middleware/validator");
+const { verifyToken } = require("../middleware/auth");
 
 router.post("/login", loginValidator, resultValidation, authController.login);
 router.post("/forgot-password", authController.forgotPassword);
+router.patch("/reset-password", verifyToken, authController.resetPassword);
 module.exports = router;
