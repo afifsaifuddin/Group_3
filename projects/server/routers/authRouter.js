005ded6433343bf60e841bbab1a041253652e5c8
Jaysons001 <Jasonyongker866@gmail.com>
2023-07-29 20:14:14 +0700

    Ini yang benar

--- projects/server/routers/authRouter.js
+++ projects/server/routers/authRouter.js
@@ -2,22 +2,16 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controller/authController");
 
-const { loginValidator, resultValidation } = require("../middleware/validator");
+const {
+  loginValidator,
+  regisValidator,
+  resultValidation,
+} = require("../middleware/validator");
 const { verifyToken } = require("../middleware/auth");
 
 router.post("/login", loginValidator, resultValidation, authController.login);
 router.post("/forgot-password", authController.forgotPassword);
 router.patch("/reset-password", verifyToken, authController.resetPassword);
-
-const {
-  loginValidator,
-  resultValidation,
-  regisValidator,
-} = require("../middleware/validator");
-const { checkrole } = require("../middleware/checkrole");
-
-router.post("/login", loginValidator, resultValidation, authController.login);
-router.post("/forgot-password", authController.forgotPassword);
 router.post(
   "/register",
   regisValidator,
