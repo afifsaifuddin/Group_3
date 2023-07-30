266132b24fa3ce38c857fb35f41e2bb8cfe54117
afifsaifuddin <55240789+afifsaifuddin@users.noreply.github.com>
2023-07-30 18:39:12 +0700

    v0.1 (#12)
    Login, reset-password, forgot

---
+++ projects/client/src/pages/dashboardadmin.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+export const Dashboardadmin = () => {
+  return (
+    <Box>
+      <h1>ini dashboard admin</h1>
+    </Box>
+  );
+};
+
+export default Dashboardadmin;
