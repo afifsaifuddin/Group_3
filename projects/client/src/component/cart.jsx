9d02ba50da8e42c490da95f22f8c33aebcc30a39
afifsaifuddin <55240789+afifsaifuddin@users.noreply.github.com>
2023-07-31 15:42:24 +0700

    fe
    perbaikan reset password

---
+++ projects/client/src/component/cart.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+export const Cart = () => {
+  return (
+    <Box>
+      <h3>ini keranjang</h3>
+    </Box>
+  );
+};
+export default Cart;
