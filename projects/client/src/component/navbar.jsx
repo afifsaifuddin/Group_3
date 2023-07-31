c088d00a1238f55bae3b13be95593057253a6e03
Jaysons001 <132377234+Jaysons001@users.noreply.github.com>
2023-07-31 14:46:17 +0700

    Fe badge (#17)
    * Tambah Badge
    
    * Perbaiki Login, dan masukin badge
    
    Masukin Badge

--- projects/client/src/component/navbar.jsx
+++ projects/client/src/component/navbar.jsx
@@ -1,12 +1,17 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Badge, Box, Flex, Text } from "@chakra-ui/react";
 import React from "react";
 import Searchbar from "./searchbar";
+import AvatarKasir from "./badge";
 export const Navbar = () => {
   return (
     <Box bgColor={"red"}>
-      <Flex justifyContent={"space-between"} p={3}>
+      <Flex
+        justifyContent={"space-between"}
+        p={3}
+      >
         <Text>Logo Magfirah Cell</Text>
         <Searchbar />
+        <AvatarKasir />
       </Flex>
     </Box>
   );
