86508656ebe0ae2ad57886e287916a71f9c337c7
Jaysons001 <Jasonyongker866@gmail.com>
2023-08-04 15:45:10 +0700

    Ini bukan dark

--- projects/server/controller/productController.js
+++ projects/server/controller/productController.js
@@ -46,25 +46,10 @@ const productController = {
 
   uploadProduk: async (req, res) => {
     try {
-      const {
-        name,
-        categoryId,
-        description,
-        modal_produk,
-        harga_produk,
-        quantity,
-      } = req.body;
+      const { name, categoryId, description, modal_produk, harga_produk, quantity } = req.body;
       console.log(req.file);
-      if (!req.file)
-        return res.status(400).json({ message: "file gamebar harus ada" });
-      console.log(
-        name,
-        categoryId,
-        description,
-        modal_produk,
-        harga_produk,
-        quantity
-      );
+      if (!req.file) return res.status(400).json({ message: "file gamebar harus ada" });
+      console.log(name, categoryId, description, modal_produk, harga_produk, quantity);
       const result = await product.create({
         name,
         categoryId,
@@ -83,15 +68,7 @@ const productController = {
 
   updateProduk: async (req, res) => {
     try {
-      const {
-        name,
-        categoryId,
-        description,
-        modal_produk,
-        harga_produk,
-        quantity,
-        isActive,
-      } = req.body;
+      const { name, categoryId, description, modal_produk, harga_produk, quantity, isActive } = req.body;
       const item = await product.findOne({ where: { id: req.params.id } });
       const updateClause = {};
       if (name) updateClause.name = name;
@@ -103,8 +80,7 @@ const productController = {
       if (isActive) updateClause.isActive = isActive;
       if (req.file) {
         fs.unlink(item.productImg, (err) => {
-          if (err)
-            res.status(500).json({ message: "Ubah gambar ada yang salah" });
+          if (err) res.status(500).json({ message: "Ubah gambar ada yang salah" });
         });
         updateClause.productImg = req.file.path;
       }
@@ -119,13 +95,8 @@ const productController = {
     const { id } = req.params;
     try {
       db.sequelize.transaction(async (t) => {
-        await product.update(
-          { isActive: false },
-          { where: { id } },
-          { transaction: t }
-        );
-        if (isActive)
-          return res.status(200).json({ message: "produk telah diaktifkan" });
+        await product.update({ isActive: false }, { where: { id } }, { transaction: t });
+        if (isActive) return res.status(200).json({ message: "produk telah diaktifkan" });
 
         return req.status(200).json({ message: "produk telah dinonaktifkan" });
       });
