ff58fcb8fda59ccc6cd15798d74fdbe2e847fe72
afifsaifuddin <55240789+afifsaifuddin@users.noreply.github.com>
2023-08-04 10:38:50 +0700

    FE-Search (#33)
    filter by asc,desc,category

--- projects/client/src/component/badge.jsx
+++ projects/client/src/component/badge.jsx
@@ -1,14 +1,23 @@
-import { Avatar, Box, Button, Flex, Stack, useDisclosure } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Flex,
+  Stack,
+  useDisclosure,
+} from "@chakra-ui/react";
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { ChangeAvatar } from "./changeAvatar";
+import ColorModeToggle from "./colorMode";
 
 export const AvatarKasir = () => {
   const { user } = useSelector((state) => state.authreducer);
   const { isOpen, onClose, onOpen } = useDisclosure();
-  
+
   let imgProfile;
-  if (user.imgProfile) imgProfile = "http://localhost:8000/" + user.imgProfile.replace(/\\/g, "/");
+  if (user.imgProfile)
+    imgProfile = "http://localhost:8000/" + user.imgProfile.replace(/\\/g, "/");
   return (
     <Box>
       <Flex
@@ -18,16 +27,23 @@ export const AvatarKasir = () => {
         w={"100%"}
         justifyContent={"left"}
         bgColor="#FC2947"
-        color={"white"}>
+        color={"white"}
+      >
         <Button
           ml={"10px"}
           variant={"unstyled"}
           onClick={() => {
             onOpen();
-          }}>
+          }}
+        >
           <Avatar color="white" name={user.username} src={`${imgProfile}`} />
         </Button>
-        <Stack ml={"15px"} fontSize={"sm"} spacing={"0.5px"} alignContent={"flex-end"}>
+        <Stack
+          ml={"15px"}
+          fontSize={"sm"}
+          spacing={"0.5px"}
+          alignContent={"flex-end"}
+        >
           <Box>Welcome, {user.role}</Box>
           <Box>{user.username}</Box>
         </Stack>
--- projects/client/src/component/produk.jsx
+++ projects/client/src/component/produk.jsx
@@ -1,17 +1,8 @@
-import {
-  Box,
-  Flex,
-  Stack,
-  Tab,
-  TabList,
-  TabPanel,
-  TabPanels,
-  Tabs,
-} from "@chakra-ui/react";
+import { Box, Flex, Select, Stack } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import CardProduk from "./produkCard.";
 import { useDispatch, useSelector } from "react-redux";
-import { getProduk } from "../redux/reducer/produkreducer";
+import { getCategory, getProduk } from "../redux/reducer/produkreducer";
 import Cart from "./cart";
 import { Pagination } from "./pagination";
 import Searchbar from "./searchbar";
@@ -19,39 +10,61 @@ import Searchbar from "./searchbar";
 export const Produk = () => {
   const dispatch = useDispatch();
   const [index, setIndex] = useState(1);
-  useEffect(() => {
-    dispatch(getProduk(index));
-  }, [index]);
-
   const { page } = useSelector((state) => state.produkreducer);
   const produk = useSelector((state) => state.produkreducer.produk);
   const category = useSelector((state) => state.produkreducer.category);
+
+  const handleCategory = (e) => {
+    const category = e.target.value;
+    dispatch(getProduk({ category }));
+  };
+  const handleOrderBy = (e) => {
+    const order = e.target.value;
+    dispatch(getProduk({ order }));
+  };
+  useEffect(() => {
+    dispatch(getProduk({ index }));
+    dispatch(getCategory());
+  }, [index]);
+
   return (
     <Flex mt={"5px"} ml={"20px"} justifyContent={"space-between"}>
       <Box>
         <Stack>
-          <Flex>
-            <Searchbar />
-            <Tabs variant="soft-rounded" colorScheme="red">
-              <TabList>
-                <Tab>Tab 1</Tab>
-                <Tab>Tab 2</Tab>
-              </TabList>
-              <TabPanels>
-                <TabPanel>
-                  <p>one!</p>
-                </TabPanel>
-                <TabPanel>
-                  <p>two!</p>
-                </TabPanel>
-              </TabPanels>
-            </Tabs>
+          <Flex gap={"5px"} justifyContent={"space-between"} width={"100%"}>
+            <Box width={"28vw"}>
+              <Searchbar />
+            </Box>
+            <Select
+              placeholder="Pilih Kategori"
+              width={"20vw"}
+              focusBorderColor="#FC2947"
+              onChange={handleCategory}
+            >
+              {category.map((item) => (
+                <option key={item.id} value={item.id}>
+                  {item.name}
+                </option>
+              ))}
+            </Select>
+            <Select
+              width={"20vw"}
+              placeholder="Urutkan Berdasarkan"
+              focusBorderColor="#FC2947"
+              onChange={handleOrderBy}
+            >
+              {/* <option value={"harga_tertinggi"}>Harga Tertinggi</option>
+              <option>Harga Terendah</option> */}
+              <option value={"ASC"}>A - Z</option>
+              <option value={"DESC"}>Z - A</option>
+            </Select>
           </Flex>
           <Flex wrap={"wrap"} gap={"20px"}>
-            {produk &&
-              produk.map((item) => <CardProduk key={item.id} produk={item} />)}
+            {produk.map((item) => (
+              <CardProduk key={item.id} produk={item} />
+            ))}
           </Flex>
-          <Box position={"absolute"} mt={"75vh"} ml={"50vh"}>
+          <Box position={"absolute"} mt={"80vh"} ml={"50vh"}>
             <Pagination page={page} index={index} setIndex={setIndex} />
           </Box>
         </Stack>
